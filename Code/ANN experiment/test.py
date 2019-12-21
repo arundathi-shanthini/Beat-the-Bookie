5805aee9425be9c4ee29cea447f52c8f48b918ae
Leykoo <dimaleyko@googlemail.com>
2019-12-21 16:41:19 +0000

    Does not converge

--- Code/ANN experiment/test.py
+++ Code/ANN experiment/test.py
@@ -8,6 +8,7 @@ import torch.nn.functional as F
 import torch.optim as optim
 import numpy as np
 
+num_epochs = 100
 
 
 
@@ -15,10 +16,11 @@ class NeuralNetworkCalculator(nn.Module):
     def __init__(self):
         super(NeuralNetworkCalculator, self).__init__()
         self.layer_1 = torch.nn.Linear(72, 36)
-        self.layer_2 = torch.nn.Linear(36, 18)
-        self.layer_3 = torch.nn.Linear(18, 18)
-        self.layer_4 = torch.nn.Linear(18, 9)
-        self.layer_5 = torch.nn.Linear(9, 3)
+        self.layer_2 = torch.nn.Linear(36, 36)
+        self.layer_3 = torch.nn.Linear(36, 18)
+        self.layer_4 = torch.nn.Linear(18, 18)
+        self.layer_5 = torch.nn.Linear(18, 9)
+        self.layer_6 = torch.nn.Linear(9, 3)
 
     def forward(self, x):
         x = F.relu(self.layer_1(x))
@@ -26,12 +28,13 @@ class NeuralNetworkCalculator(nn.Module):
         x = F.relu(self.layer_3(x))
         x = F.relu(self.layer_4(x))
         x = F.relu(self.layer_5(x))
+        x = F.relu(self.layer_6(x))
         return x
 
 net = NeuralNetworkCalculator()
 
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(net.parameters(), lr = 0.0001)
+criterion = nn.MSELoss()
+optimizer = optim.Adam(net.parameters(), lr = 0.00001)
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 net.to(device)
@@ -43,21 +46,21 @@ hometeam = training_data['HomeTeam']
 awayteam = training_data['AwayTeam']
 
 
-
 HT = torch.tensor(pd.get_dummies(hometeam).values)
 AT = torch.tensor(pd.get_dummies(awayteam).values)
-W = torch.tensor(pd.get_dummies(win).values)
-HAT = torch.transpose(torch.cat((torch.transpose(HT, 0, 1),torch.transpose(AT, 0, 1)),0), 0, 1)
-print(HAT.size())
+W = torch.tensor(pd.get_dummies(win).values).float()
+HAT = torch.transpose(torch.cat((torch.transpose(HT, 0, 1),torch.transpose(AT, 0, 1)),0), 0, 1).float()
 
+total_step = 4180
 
-
-"""for epoch in range(2):  # loop over the dataset multiple times
+for epoch in range(num_epochs):  # loop over the dataset multiple times
     running_loss = 0.0
-    for i in range(0, x.shape[0]):
+    for i in range(0, HAT.shape[0]):
         # get the inputs
-        inputs, labels = x[i], y[i]
-
+        inputs, labels = HAT[i], W[i]
+        """mu, sigma = 0, 0.01
+        noise = torch.from_numpy(np.random.normal(mu, sigma, [1,72])).float()
+        inputs = inputs + noise"""
         # zero the parameter gradients
         optimizer.zero_grad()
 
@@ -69,7 +72,7 @@ print(HAT.size())
 
         # print statistics
         running_loss += loss.item()
-        if i % 200 == 199:
-            print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 200))
+        if (i+1) % 1000 == 0:
+            print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'.format(epoch+1, num_epochs, i+1, total_step, loss.item()))
             running_loss = 0.0
-print('Finished Training')"""
+print('Finished Training')
