9b9239fe45d6a5761317f1f2412afe3e1d769247
Leykoo <dimaleyko@googlemail.com>
2019-12-21 12:55:05 +0000

    test

--- Code/ANN experiment/test.py
+++ Code/ANN experiment/test.py
@@ -1,3 +1,16 @@
+import numpy as np
 import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import torch
 
-pd.load_csv()
+
+
+training_data=pd.read_csv('epl-training.csv')
+
+win = training_data['FTR']
+
+print(training_data)
+print(win)
+training = torch.tensor(training_data.values)
+print(training)
