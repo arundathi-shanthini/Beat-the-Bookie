dd3a7e7a41b80c8441bd06f6ebeca5a1864ef439
Leykoo <dimaleyko@googlemail.com>
2020-01-04 14:58:11 +0000

    Location csv

--- Code/ANN experiment/getweather.py
+++ Code/ANN experiment/getweather.py
@@ -15,21 +15,37 @@ def get_weather(api_key, location, time):
     r = requests.get(url)
     return r.json()
 
-def get_time(date):
-    d = datetime.date(2015,1,5)
-    unixtime = time.mktime(d.timetuple())
+def get_time(data):
+    T = []
+    unixtime = []
+    Y = pd.to_datetime(data['Date'],format='%d/%m/%Y').dt.year
+    M = pd.to_datetime(data['Date'],format='%d/%m/%Y').dt.month
+    D = pd.to_datetime(data['Date'],format='%d/%m/%Y').dt.day
+    for i in range(len(data.index)):
+        T.append(datetime.date(Y[i],M[i],D[i]))
+        unixtime.append(time.mktime(T[i].timetuple()))
     return unixtime
+
+def get_location(data):
+    HtoL = pd.read_csv('HomeTeamLocation.csv')
+    H = data['HomeTeam']
+    L =[]
+    print(set(data['HomeTeam'].unique())-set(HtoL['HomeTeam'].unique()))
+    print(HtoL)
 
 def main():
     training_data=pd.read_csv('epl-training.csv')
 
     location = 'London'
-
-    api_key = get_api_key()
+    training_data['UT'] = get_time(training_data)
+    print(training_data)
+    print(training_data['HomeTeam'].nunique())
+    get_location(training_data)
+    """api_key = get_api_key()
     weather = get_weather(api_key, location, get_time(1))
-    print(get_time(1))
+
     #print(weather['main']['temp'])
-    print(weather)
+    print(weather)"""
 
 
 if __name__ == '__main__':
